f31bbf59804babe043eb4e14da2ba2e4bfb97758
omarhaqqi24 <muhammadomarhaqqi@gmail.com>
2026-05-07 23:23:04 +0700

    Added: Proteksi peak load and Updated: README.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# capstone-b4--kel5
+# Capstone-b4--kel5
+## Penggunaan
+
+Build container:
+
+```
+cd capstone-cimb
+docker compose up -d --build
+```
+---
+Cara menjalankan program (default 1 worker-service dan 1 api-service):
+```
+docker compose up -d
+```
+---
+Cara menjalankan program dengan jumlah worker-service dan api-service tertentu (contoh 3 api-service dan 7 worker-service):
+```
+docker compose up --scale api-service=3 --scale worker-service=7 -d
+```
+---
+Setiap ada perubahan program, build ulang container:
+```
+docker compose down
+docker compose up -d --build
+
+# Jika mau langsung beberapa api-service atau worker-service:
+
+docker compose up --scale api-service=3 --scale worker-service=7 -d --build
+```
--- capstone-cimb/services/api-python/main.py
+++ capstone-cimb/services/api-python/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 import redis
 import psycopg2
 import json
 import uuid
 from prometheus_fastapi_instrumentator import Instrumentator
+import time
+from redis.exceptions import TimeoutError
 
 app = FastAPI(title="CIMB Peak Load API")
-redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(
+    host='redis',
+    port=6379,
+    db=0,
+    decode_responses=True,
+    socket_timeout=2,
+    socket_connect_timeout=2
+)
+
+MAX_QUEUE_SIZE = 1000
+FAILURE_THRESHOLD = 5
+RECOVERY_TIMEOUT = 10
+
+failure_count = 0
+circuit_open = False
+last_failure_time = 0
 
 # Integrasi Prometheus Metrics
 Instrumentator(
@@ -36,14 +54,67 @@ def startup_event():
 # ENDPOINT WRITE (Masuk ke Message Queue)
 @app.post("/transaction")
 async def create_transaction(user_id: int, amount: float):
+    if is_circuit_open():
+        return JSONResponse(
+            status_code=503,
+            content={
+                "status": "error",
+                "message": "Circuit breaker active"
+            }
+        )
+    
+    global failure_count, circuit_open, last_failure_time
+
+    # Backpressure Check
+    queue_size = redis_client.llen("tx_queue")
+
+    if queue_size >= MAX_QUEUE_SIZE:
+        return JSONResponse(
+            status_code=503,
+            content={
+                "queue_size": queue_size,
+                "status": "error",
+                "message": "Queue overload, try again later"
+            }
+        )
+
     tx_id = str(uuid.uuid4())
     payload = {"id": tx_id, "user_id": user_id, "amount": amount, "status": "PENDING"}
     
     # Push ke Redis Queue
-    redis_client.lpush("tx_queue", json.dumps(payload))
+    try:
+        redis_with_retry(
+            lambda: redis_client.lpush(
+                "tx_queue",
+                json.dumps(payload)
+            )
+        )
+
+        failure_count = 0  # Reset failure count on success
+
+    except Exception:
+        failure_count += 1
+
+        if failure_count >= FAILURE_THRESHOLD:
+            circuit_open = True
+            last_failure_time = time.time()
+
+        return JSONResponse(
+            status_code=503,
+            content={
+                "status": "error",
+                "message": "Redis unavailable"
+            }
+        )
     
     # Simpan status sementara di Cache agar bisa langsung di-inquiry
-    redis_client.setex(f"tx_status:{tx_id}", 300, "PENDING") 
+    redis_with_retry(
+        lambda: redis_client.setex(
+            f"tx_status:{tx_id}",
+            300,
+            "PENDING"
+        )
+    )
     
     return {"message": "Transaction queued", "tx_id": tx_id}
 
@@ -69,3 +140,32 @@ async def transaction_inquiry(tx_id: str):
         return {"tx_id": tx_id, "status": result[0], "source": "database"}
     
     raise HTTPException(status_code=404, detail="Transaction not found")
+
+def redis_with_retry(operation, retries=3, delay=1):
+    for attempt in range(retries):
+        try:
+            return operation()
+
+        except TimeoutError:
+
+            print(f"Retry attempt {attempt+1}")
+
+            if attempt == retries - 1:
+                raise
+
+            time.sleep(delay)
+
+def is_circuit_open():
+    global circuit_open, last_failure_time
+
+    if circuit_open:
+        elapsed = time.time() - last_failure_time
+
+        # HALF-OPEN setelah timeout
+        if elapsed > RECOVERY_TIMEOUT:
+            circuit_open = False
+            return False
+
+        return True
+
+    return False
